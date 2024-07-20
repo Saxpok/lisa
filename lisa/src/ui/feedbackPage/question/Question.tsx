24e51265899351e61575e6a6b5cc293fd222f5a1
Oleh <oleh.stelmashchuklv@gmail.com>
2024-07-21 00:55:21 +0300

    debug: CSS styles

--- lisa/src/ui/feedbackPage/question/Question.tsx
+++ lisa/src/ui/feedbackPage/question/Question.tsx
@@ -23,31 +23,30 @@ const Question = ({ question, ansver }: QuestionProps) => {
                 </div>
                 <div className='questionArrow'>
                     {active ?
-                        <svg
-                            width="35"
-                            height="20"
-                            viewBox="0 0 35 20"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M17.0512 14.2821L2.76908 0L0 2.76908L17.0512 19.8203L34.1024 2.76908L31.3333 0L17.0512 14.2821Z"
-                                fill="white"
-                            />
+                        <svg 
+                        width="35" 
+                        height="20" 
+                        viewBox="0 0 35 20" 
+                        fill="none" 
+                        xmlns="http://www.w3.org/2000/svg"
+                        >
+                        <path 
+                        d="M17.0512 5.53819L31.3333 19.8203L34.1024 17.0512L17.0512 2.28882e-05L-3.8147e-06 17.0512L2.76908 19.8203L17.0512 5.53819Z" 
+                        fill="white" 
+                        />
                         </svg>
                         :
-                        <svg 
-                            width="35" 
-                            height="20" 
-                            viewBox="0 0 35 20" 
-                            fill="none" 
-                            xmlns="http://www.w3.org/2000/svg"
-                            >
-                            <path 
-                            d="M17.0512 5.53819L31.3333 19.8203L34.1024 17.0512L17.0512 2.28882e-05L-3.8147e-06 17.0512L2.76908 19.8203L17.0512 5.53819Z" 
-                            fill="white" 
-                            />
-                        </svg>
-
+                        <svg
+                        width="35"
+                        height="20"
+                        viewBox="0 0 35 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M17.0512 14.2821L2.76908 0L0 2.76908L17.0512 19.8203L34.1024 2.76908L31.3333 0L17.0512 14.2821Z"
+                            fill="white"
+                        />
+                    </svg>
                     }
                 </div>
             </div>
